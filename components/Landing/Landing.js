0c763e49a2977f4e9f086aaa22064190cf1ed3cd
Chandan Kumar <cksharma122@gmail.com>
2020-05-03 14:17:43 +0530

    Initial styles setup

---
+++ components/Landing/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>Ice Cream Meter</h1>
+      <button>Create Team</button>
+      <button>Team Login</button>
+    </div>
+  );
+};
+
+export default Landing;
---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+
+class IceCreamMeterDocument extends Document {
+  render() {
+    return (
+      <html lang="es">
+        <link rel="stylesheet" type="text/css" href="/styles/base.css" />
+        <title>Ice Cream Meter</title>
+        <Head></Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
+
+export default IceCreamMeterDocument;
